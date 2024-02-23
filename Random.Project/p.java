34729d89189befb9b62d6bfd3e432367461837e9
Symphoen1x <116389179+Symphoen1x@users.noreply.github.com>
2024-02-23 08:56:17 +0700

    Add files via upload

---
+++ Random.Project/p.java
@@ -0,0 +1,30 @@
+// 
+public class p {
+        public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.print("I will print on the same line.");
+        System.out.print(" Minimal Maksimal bos\n");
+        System.out.println(3);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+ILMU BARU:
+Assalamualaikum UKHTI!!!!
+
+* Output:
+Hello Word
+I will print on the same line.
+
+Dih Najisss:(
+ */
---
+++ Random.Project/variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+
+
+
+    }
+}
