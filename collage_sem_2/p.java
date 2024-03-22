049fa4c0a3e27c3d9cc252cfee4f1cbabfa4fb20
Symphoen1x <116389179+Symphoen1x@users.noreply.github.com>
2024-03-22 09:58:55 +0700

    Add files via upload

--- collage_sem_2/p.java
+++ collage_sem_2/p.java
@@ -1,5 +1,104 @@
+// 
 public class p {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
+        public static void main(String[] args) {
+                // int n = 10; /*Define the initiation */
+                // System.out.println("Ini adalah deret ganjil: "); 
+        
+                // for (int i = 0; i <= n; i++) {
+                //         if (i % 2 == 1) {
+                //                 System.out.println(i);
+                //   }
+                // }
+                // System.out.println("Ini adalah deret genap: ");
+                // for (int c = 0; c <= n; c++) {
+                //         if (c % 2 == 0){
+                //                 System.out.println(c);
+                //  }
+                // }
+                // System.out.println("Ini adalah deret prima: ");
+                // for (int p = 2; p <= n; p++) {
+
+                // }
+                // String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+                // System.out.println(cars[0]);
+                // cars[0] = "Opel";
+                // System.out.println(cars[0]);
+
+                // // Menampilkan keseluruhan value array:
+                // for (int i = 0; i < cars.length; i++) {
+                //     System.out.println(cars[i]);
+                // int [] anArray; /* define the array initiation */
+                // anArray = new int[10];  /* define the size of array */
+                // for(int i = 0; i < anArray.length; i++){             /* define the part looping of array */
+                //         anArray[i] = i; /* define */
+                //         System.out.print(anArray[i] + " ");
+
+                // }
+                // System.out.println();
+                
+                // 4
+
+                String[][] cartoons =
+                {{ "Flintstones", "Fred", "Wilma", "Pebbles", "Dino" },
+                {"Rubbles", "Barney", "Betty", "Bam Bam" },
+                { "Jetsons", "George", "Jane", "Elroy", "Judy", "Rosie",
+                "Astro"},
+                { "Scooby Doo Gang", "Scooby Doo", "Shaggy", "Velma",
+                "Fred", "Daphne"} };
+                for (int i = 0; i < cartoons.length; i++) {
+                        System.out.print (cartoons[i] [0] + ": ");
+                        for (int j = 0; j < cartoons[i].length; j++) {
+                        System.out.print (cartoons[i] [j] + " ");
+                        System.out.println() ;
+                                
+                        }
+                  }
+
+                // 5
+                
+                // int[][] matrix = new int[4] [];
+                // //populate matrix
+                // for (int i = 0; i < matrix.length; i++) {
+                //         matrix[i] = new int[5]; //create sub-array
+                //         for (int j = 0; j < matrix[i].length; j++) {
+                //                 matrix[i] [j] = i + j;
+                //         }
+                // }        
+                // //print matrix
+                // for (int i = 0; i < matrix.length; i++) {
+                // for (int j = 0; j < matrix[i].length; j++) {
+                // System.out.print (matrix[i] [j]);
+                // }
+                // System.out.println();
+                //                 }
+                // 6
+               
+
+
+
+
+
+
+
+
+
+
+
+
+                        }                       
+                }
+
+
+
+
+
+/*
+ILMU BARU:
+Assalamualaikum UKHTI!!!!
+
+* Output:
+Hello Word
+I will print on the same line.
+
+Dih Najisss:(
+ */
